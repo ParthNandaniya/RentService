0e95f22b563b619ac7b982d293c6813720c5bbef
Parth Nandaniya <30195101+ParthNandaniya@users.noreply.github.com>
2018-04-10 00:13:30 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose");
+    
+var UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+   username: {
+       type: String,
+       required: true
+   },
+   password: {
+       type: String
+   },
+    terms: {
+        type: String
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
